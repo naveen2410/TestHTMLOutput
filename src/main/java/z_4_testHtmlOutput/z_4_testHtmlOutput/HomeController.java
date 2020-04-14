9a00aa5413b6e89d93b80d4314059ca368317dcb
Naveen <navegoya@in.ibm.com>
2020-04-14 12:58:03 +0530

    v2

--- src/main/java/z_4_testHtmlOutput/z_4_testHtmlOutput/HomeController.java
+++ src/main/java/z_4_testHtmlOutput/z_4_testHtmlOutput/HomeController.java
@@ -15,19 +15,43 @@
  */
 package z_4_testHtmlOutput.z_4_testHtmlOutput;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import javax.annotation.PostConstruct;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+ 
 
-@RestController
+@Controller
 public class HomeController {
 
-    @Value("${welcome}")
-    private String welcome;
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String ipaddress() throws Exception {
-        return "Reply: " + welcome;
-    }
+	List<Customer> lstCustomers = new ArrayList<Customer>();
+	  
+	  @PostConstruct
+	    public void init() throws Exception {
+	    lstCustomers.add(new Customer(new Long(1), "Jack", 25, new Address("NANTERRE CT", "77471")));
+	    lstCustomers.add(new Customer(new Long(2), "Mary", 37, new Address("W NORMA ST", "77009")));
+	    lstCustomers.add(new Customer(new Long(3), "Peter", 18, new Address("S NUGENT AVE", "77571")));
+	    lstCustomers.add(new Customer(new Long(4), "Amos", 23, new Address("E NAVAHO TRL", "77449")));
+	    lstCustomers.add(new Customer(new Long(5), "Craig", 45, new Address("AVE N", "77587")));
+	    lstCustomers.add(new Customer(new Long(6), "Aries", 19, new Address("Broadway/Reade St, New York", "10007")));
+	    lstCustomers.add(new Customer(new Long(7), "Brice", 39, new Address("Columbus, OH 43215, USA", "43215")));
+	    lstCustomers.add(new Customer(new Long(8), "Cage", 24, new Address("Plano, TX 75074", "75074")));
+	    lstCustomers.add(new Customer(new Long(9), "Ellen", 41, new Address("Modesto, CA 95354", "95354")));
+	    lstCustomers.add(new Customer(new Long(10), "Brice", 32, new Address("Atlanta, GA 30334", "30334")));
+	    }
+	 
+	  @GetMapping
+	  public String index() {
+	    return "redirect:/customer";
+	  }
+	  
+	  @GetMapping("/customer")
+	    public String getCustomer(Model model) {
+	        model.addAttribute("customers", lstCustomers);
+	        return "customer/customer.table.html";
+	    }
 }
